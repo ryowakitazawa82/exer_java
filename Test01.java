a99508d60212fc55eb067e1dd29b38cc7e6515c6
ryowa.kitazawa <88272934+ryowakitazawa82@users.noreply.github.com>
2022-03-04 20:46:03 +0900

    first commit

---
+++ Test01.java
@@ -0,0 +1,9 @@
+public class Test01 {
+	public static void main(String[] args) {
+		boolean result = true;
+		char a = '駆'; //１文字の場合はシングルクォーテーション
+		final double pi = 3.14; //「final」を付けると、上書きを不可能にできる。piなど値が変わらないものにつける。
+		long foreverLove = 314159265853979L;
+		String message = "あああ！！"; //ダブルクォーテーションで囲む
+	}
+}
